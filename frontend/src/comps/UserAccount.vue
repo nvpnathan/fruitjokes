dad46741b9f6f944ca0c60350c76e1b0cc3eaaca
Nathan Ness <nathan79@gmail.com>
2022-09-29 14:53:26 -0700

    add frontend framework

---
+++ frontend/src/comps/UserAccount.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-row>
+    <p>Show user account details</p>
+  </el-row>
+  <el-row>
+    <p>Token: {{ user.access_token }}</p>
+  </el-row>
+  <el-row>
+    <p>User: {{ user.username }}</p>
+  </el-row>
+  <el-row>
+    <Logout />
+  </el-row>
+</template>
+
+<script>
+import Logout from './Logout.vue';
+import { mapGetters } from 'vuex'
+
+export default {
+  components: {
+    Logout,
+  },
+  computed: {
+    ...mapGetters(['auth/user'])
+  },
+};
+</script>
+
+<style>
+</style>
