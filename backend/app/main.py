affa67431527d0f45aa94765e0853178dfab742c
Nathan Ness <nathan79@gmail.com>
2022-12-27 10:01:22 -0700

    fixed dockerfiles

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,4 +22,4 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import { createApp } from 'vue'
+import axios from 'axios';
+
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:5000'; //FastAPI backend
+
+app.use(router);
+app.mount("#app");
+
+
+
+
+
