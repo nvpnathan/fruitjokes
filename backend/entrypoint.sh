f08ee083bd87eca2e54e3b8764a48aec7667eb8c
Nathan Ness <nathan79@gmail.com>
2023-01-05 21:30:30 -0700

    added initialize db

---
+++ backend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Initalizing the DB
+if $INITIALIZE_DB = true; then
+    echo "Initializing Database"
+    aerich init -t app.core.config.TORTOISE_ORM
+    aerich init-db
+fi
+
+# Migrating the DB
+if $MIGRATE_DB = true; then
+    echo "Migrating Database"
+    aerich migrate
+    aerich upgrade
+fi
+
+sleep 5s
